2bf2bb66220dd8f5ae5d0566bf6a7b7f6ea1fd92
Uygar KAYA <uygar.kaya@ozu.edu.tr>
2021-05-14 22:11:56 +0300

    mem1R1W.v is added

---
+++ LedCPUwithRAM/mem1R1W.v
@@ -0,0 +1,29 @@
+module mem1R1W(
+		clk,
+		addrRd,
+		dataRd,
+		addrWr,
+		dataWr,
+		wrEn
+);
+input clk;
+input wrEn;
+input [7:0] addrRd;
+input [7:0] addrWr;
+input [15:0] dataWr;
+output reg [15:0] dataRd;
+
+reg [15:0] array [0:255];
+
+initial begin
+    `include "init.v"
+end
+
+always@(posedge clk) begin
+	dataRd <= array[addrRd];
+	if(wrEn)
+		array[addrWr] <= dataWr;
+end
+
+endmodule
+
