bb6f34699c235b0276cbb4edcaa8015cea377536
Uygar KAYA <uygar.kaya@ozu.edu.tr>
2021-07-02 01:31:47 +0300

    tb_CombiCircuit file is added

---
+++ CombiCircuit/tb_CombiCircuit.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+module tb_CombiCircuit();
+	
+	// inputs
+	reg a,b,c;
+	
+	// outputs
+	wire y;
+	
+	// instantiate the unit under test (UTT)
+	CombiCircuit uut (.a(a), .b(b), .c(c), .y(y));
+	
+	// initialize the inputs
+	initial begin 
+	
+	a = 0;
+	b = 0;
+	c = 0;
+	
+	#50 // waiting 50 ns. ns--> Nano Second.
+	
+	a = 0;
+	b = 0;
+	c = 1;
+	
+	#50 // waiting 50 ns. ns--> Nano Second.
+	
+	a = 1;
+	b = 0;
+	c = 0;
+	
+	#50 // waiting 50 ns. ns--> Nano Second.
+	
+	a = 1;
+	b = 1;
+	c = 0;
+	
+	#50	// waiting 50 ns. ns--> Nano Second.
+	
+	$finish; // Stop the simulation
+	end 
+endmodule
